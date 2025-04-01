66a69bc6a97104b8516729dc6b6966acb67af6fb
EarlyXS <shm02156@gmail.com>
2025-04-01 16:14:59 +0900

    trying to remove original fonts

--- accessed/src/pages/home/home.js
+++ accessed/src/pages/home/home.js
@@ -2,10 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-        <h1> 야 너두 사장될 수 있어!</h1>
-        <p> 멋쟁이 사자처럼 13기 </p>
-        </div>
+        <div></div>
     )
 }
 
